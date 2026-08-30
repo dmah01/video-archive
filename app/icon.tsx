0b1b7c717897e3afc20985236c47a9f4e8e2cfb1
dmah01 <a40310600@gmail.com>
2026-08-30 11:54:38 +0900

    Update website

--- app/icon.tsx
+++ app/icon.tsx
@@ -11,12 +11,13 @@ export default function Icon() {
   return new ImageResponse(
     <div
       style={{
-        fontSize: 48,
         width: "100%",
         height: "100%",
         display: "flex",
         alignItems: "center",
         justifyContent: "center",
+        fontSize: 64,
+        lineHeight: 1,
       }}
     >
       🐳
